46037660347fd7219d406ae3875254e24ef75e52
Student <student@alpha>
2017-02-22 15:47:33 -0600

    quotes

---
+++ quotes-data.lisp
@@ -0,0 +1,7 @@
+(("This war did not spring up on our land, this war was brought upon
+      us by the children of the Great Father who came to take our land
+      without a price, and who, in our land, do a great many evil
+      things... This war has come from robbery - from the stealing of
+      our land."
+  "Spotted Tail"
+  "Lakota Sioux Chief"))
