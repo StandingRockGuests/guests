47e1e441413a90899cac1fc1ecac40516bce703f
Student <student@alpha>
2017-03-07 06:04:01 -0700

    add Red Cloud quote

--- data/quotes-data.lisp
+++ data/quotes-data.lisp
@@ -4,4 +4,11 @@
       things... This war has come from robbery - from the stealing of
       our land."
   "Spotted Tail"
-  "Lakota Sioux Chief"))
+  "Lakota Elder")
+ ("We told them that the supernatural powers, Táku Wakȟáŋ, had given to the Lakȟóta, the
+buffalo for food and clothing. We told them that where the buffalo ranged, that was
+our country. We told them that the country of the buffalo was the country of the
+Lakȟóta. We told them that the buffalo must have their country and the Lakȟóta must
+have the buffalo."
+  "Red Cloud"
+  "Lakota Elder"))
