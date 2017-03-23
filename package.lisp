c02502abef067e5a579023a9512112a358cfd59a
Student <student@alpha>
2017-03-23 05:35:24 -0600

    import console

--- package.lisp
+++ package.lisp
@@ -1,6 +1,7 @@
 
 (defpackage guests
   (:use common-lisp story iterate cl-who cl-css cl-ascii-art split-sequence)
+  (:import-from story-js console)
   (:import-from uiop directory-files)
   (:import-from alexandria if-let)
   (:import-from asdf/run-program run-program))
