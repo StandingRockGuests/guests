77faef4c627fd3cfabeabcfd84a655a71ef5742a
Student <student@alpha>
2017-03-25 10:19:31 -0600

    improting from story

--- utility.lisp
+++ utility.lisp
@@ -1,8 +1,5 @@
 (in-package :guests)
 
-(defun asdf-base-path (name)
-  (directory-namestring (asdf:component-pathname (asdf:find-system name))))
-
 (defun guests-file (&optional  base)
   (concatenate 'string (asdf-base-path :guests) base))
 
